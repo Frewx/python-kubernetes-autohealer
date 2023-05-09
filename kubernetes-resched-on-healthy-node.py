f8640a1822a1b780d58207b77a9a06b3287f8494
Frewx <hikmetugurakgul@gmail.com>
2023-05-09 16:39:03 +0300

    Major changes to algorithm and functions in kubernetes-resched-on-healthy-node.py

--- kubernetes-resched-on-healthy-node.py
+++ kubernetes-resched-on-healthy-node.py
@@ -1,47 +1,68 @@
-from kubernetes import client, config, watch
+from kubernetes import client, config
 import time
 
 # Load Kubernetes configuration
 config.load_kube_config()
 
-# Create Kubernetes API client
-v1 = client.CoreV1Api()
+def delete_pods(node_name,EVICTION_GRACE_PERIOD_SECONDS):
+    # Create the Kubernetes API client
+    v1 = client.CoreV1Api()
 
-# Define the node failure threshold (in seconds)
-threshold = 300
+    # Define a label selector to select only pods that don't have the k8s-app label
+    label_selector = f"spec.nodeName={node_name},!k8s-app"
 
-# Watch for node events
-nodes = v1.list_node().items
-
-for node in nodes:
-    node_name = node.metadata.name
-    node_status = node.status.conditions[-1]
-
-    node_last_heartbeat = int(time.time()) - int(node.status.conditions[-1].last_heartbeat_time.timestamp())
-
-    print("Last heartbeat for node {0} is {1}".format(node.metadata.name,node_last_heartbeat))
-
-    # Check if the node is unresponsive
-    if node_last_heartbeat > threshold:
-
-        print("Node {0} is unresponsive".format(node.metadata.name))
-        # Get all pods running on the affected node
-        pods = v1.list_pod_for_all_namespaces(field_selector=f"spec.nodeName={node.metadata.name}").items
-
+    # Get all the pods that match the labels running on the unresponsive node
+    pods = v1.list_pod_for_all_namespaces(field_selector=f"spec.nodeName={node_name}",label_selector=label_selector).items
+    
+    if len(pods) > 0:
+        # For each pod, delete the pod with grace period
         for pod in pods:
-            print("Pods on the responsive nodes are {0} rescheduling on a healthy node...".format(pod.metadata.name))
-
-        # # Reschedule the pods on healthy nodes
-        # for pod in pods:
-        #     if pod.status.phase == "Running":
-        #         pod_name = pod.metadata.name
-        #         pod_namespace = pod.metadata.namespace
-
-        #         # Find a healthy node to reschedule the pod on
-        #         for node in v1.list_node().items:
-        #             if node.status.conditions[-1].status == "True":
-        #                 pod.spec.node_name = node.metadata.name
-        #                 v1.patch_namespaced_pod(pod_name, pod_namespace, pod)
-        #                 break
+            print("Deleting pod {0}".format(pod.metadata.name))
+            v1.delete_namespaced_pod(
+                pod.metadata.name,
+                pod.metadata.namespace,
+                body=client.V1DeleteOptions(grace_period_seconds=EVICTION_GRACE_PERIOD_SECONDS, propagation_policy='Background')
+            )
     else:
-        print("Node {0} is responsive".format(node.metadata.name))
+        print("There is no pod on unresponsive node")
+
+def check_node_status(node_name):
+    # Create the Kubernetes API client
+    v1 = client.CoreV1Api()
+    # Get the node status
+    node = v1.read_node_status(node_name)
+
+    # Check if the node is ready
+    for condition in node.status.conditions:
+        if condition.type == "Ready" and condition.status != "True":
+            return False
+
+    return True
+
+def search_unresponsive_nodes():
+    # Eviction grace period in seconds
+    EVICTION_GRACE_PERIOD_SECONDS = 0 # NOTE: ugurakgul - we are forcing the delete
+
+    # Create the Kubernetes API client
+    v1 = client.CoreV1Api()
+    # Get all the nodes
+    nodes = v1.list_node().items
+
+    for node in nodes:
+        # Check if the node is unresponsive
+        if not check_node_status(node.metadata.name):
+            print("Node: {0} is not responsive".format(node.metadata.name))
+            # Forcefully delete the pods from the unresponsive node
+            delete_pods(node.metadata.name,EVICTION_GRACE_PERIOD_SECONDS) 
+            # When we delete the pods, because of the replicas, new pods will be deployed on the responsive nodes
+
+def main():
+    # Define the check interval seconds
+    CHECK_INTERVAL_SECONDS = 10
+
+    while True:
+        search_unresponsive_nodes()
+        time.sleep(CHECK_INTERVAL_SECONDS)
+
+if __name__ == "__main__":
+    main()
